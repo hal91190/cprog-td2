4e1d984e1e3177a0fd15efb2400242ee7fdf814b
Stéphane Lopes <stephane.lopes@uvsq.fr>
2022-10-13 12:20:16 +0200

    Complète les tests de création

--- src/test/java/fr/uvsq/cprog/mvnjunit/CipheredStringTest.java
+++ src/test/java/fr/uvsq/cprog/mvnjunit/CipheredStringTest.java
@@ -1,6 +1,7 @@
 package fr.uvsq.cprog.mvnjunit;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,9 +10,14 @@ public class CipheredStringTest {
     public static final int SHIFT = 1;
     public static final String TOSTRING = "HAL, 1 ->";
 
-    @Test
-    public void shouldCreateACipheredString() {
-        CipheredString cs = new CipheredString(MESSAGE, SHIFT);
-        assertEquals(TOSTRING, cs.toString());
+    @ParameterizedTest
+    @CsvSource({
+      "HAL, 1, 'HAL, 1 ->'",
+      "'', 1, ', 1 ->'",
+      "null, 1, 'null, 1 ->'"
+    })
+    public void shouldCreateACipheredString(String message, int shift, String expected) {
+        CipheredString cs = new CipheredString(message, shift);
+        assertEquals(expected, cs.toString());
     }
 }
